4db4f4de2e393fa4498ef1fb06b6e25b79aeac30
wujia <wzsayiie@qq.com>
2021-10-22 11:58:33 +0800

    split out project configurations.

---
+++ u3dmobile/Assets/Game/ProjectConfig/ProjectConfig.cs
@@ -0,0 +1,10 @@
+namespace U3DMobile
+{
+    public static class ProjectConfig
+    {
+        public const string StaticFGUIAssetDir   = "Assets/Resources/fairygui";
+
+        public const string BundleManuscriptFile = "Assets/Bundle/manuscript/manuscript.js";
+        public const string BundleFGUIAssetDir   = "Assets/Bundle/fairygui";
+    }
+}
